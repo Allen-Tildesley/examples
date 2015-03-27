5384eeaa9d2696a188c64c4d31a75769f61ddf95
Mike <m.p.allen@warwick.ac.uk>
2015-03-27 16:33:53 +0000

    first example files

---
+++ pbc_rd.f90
@@ -0,0 +1,28 @@
+! pbc_rd.f90
+SUBROUTINE pbc_rd ( r )
+  ! Periodic boundary conditions for rhombic dodecahedron
+
+  ! The box is centred at the origin. 
+  ! The x and y axes join the centres of opposite faces of the dodecahedron.
+  ! The z axis joins opposite vertices of the dodecahedron.
+  ! The diagonal of the rhombic face is of unit length.
+  ! The side of the containing cube is sqrt(2.0).
+  ! The x and y axes pass through the cube edges.
+  ! The z axis passes through the cube faces.
+  ! Ref: Smith W, CCP5 quarterly, 10, 37 (1983).
+
+  IMPLICIT NONE
+  REAL, DIMENSION(3), INTENT(inout) :: r ! Argument
+
+  REAL            :: corr
+  REAL, PARAMETER :: rt2 = SQRT(2.0), rrt2 = 1.0 / rt2
+
+  r(1) = r(1) - ANINT ( r(1) )
+  r(2) = r(2) - ANINT ( r(2) )
+  r(3) = r(3) - rt2 * ANINT ( rrt2 * r(3) )
+  corr = 0.5 * AINT ( ABS ( r(1) ) + ABS ( r(2) ) + rt2 * ABS ( r(3) ) )
+  r(1) = r(1) - SIGN ( corr, r(1) )
+  r(2) = r(2) - SIGN ( corr, r(2) )
+  r(3) = r(3) - SIGN ( corr, r(3) ) * rt2
+
+END SUBROUTINE pbc_rd
---
+++ pbc_to.f90
@@ -0,0 +1,21 @@
+! pbc_to.f90
+SUBROUTINE pbc_to ( r )
+  ! Periodic boundary conditions for truncated octahedron
+
+  ! The box is centred at the origin. 
+  ! The containing cube is of unit length.
+  ! The axes pass through the centres of 
+  ! the six square faces of the truncated octahedron.
+  ! Ref: Adams DJ, CCP5 quarterly, 10, 30 (1983)
+
+  IMPLICIT NONE
+  REAL, DIMENSION(3), INTENT(inout) :: r ! Argument
+
+  REAL               :: corr
+  REAL, PARAMETER    :: r75 = 4.0 / 3.0
+
+  r(:) = r(:) - ANINT ( r(:) )
+  corr = 0.5 * AINT ( r75 * SUM ( ABS ( r(:) ) ) )
+  r(:) = r(:) - SIGN ( corr, r(:) )
+
+END SUBROUTINE pbc_to
