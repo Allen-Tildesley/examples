6210cc0be2827a9d6ec43ff47deea5787c7200cf
LonelyProf <prof.mike.allen@gmail.com>
2016-06-20 17:12:33 +0100

    completed work on DJT qmc program

--- qmc_walk_sho.f90
+++ qmc_walk_sho.f90
@@ -1,179 +1,187 @@
 ! qmc_walk_sho.f90
 ! Quantum Monte Carlo, random walk, simple harmonic oscillator
 PROGRAM qmc_walk_sho
-  !
-  ! Program to calculate the ground state wavefunction for a particle in a harmonic potential, V=(x^2)/2,
-  ! by solving the corresponding diffusion equation in imaginary time.
-  !
-  ! Run the simulation with et = 0.5, the exact groundstate energy for this potential.
-  ! Then try et = 0.6 and et = 0.4 observing the behaviour of the number of walkers in each case.
-  ! This type of simulation is very senstive to the intial guess for et.
-  ! Finally compare the simulated ground state wave function for the exact result for this simple problem.
-
 
   USE utility_module, ONLY: random_normal
   IMPLICIT NONE
 
-  REAL,    DIMENSION(:), ALLOCATABLE  :: x          ! position of each walker (n_max)
-  INTEGER, DIMENSION(:), ALLOCATABLE  :: replica    ! number of replicas to make (n_max)
-  INTEGER, DIMENSION(:), ALLOCATABLE  :: bin        ! histogram bins for wavefunction (nbin)
-  REAL,    DIMENSION(:), ALLOCATABLE  :: psi        ! wavefunction (nbin)
-  REAL, PARAMETER                     :: diff = 0.5 ! diffusion coefficient in atomic units
+  ! Program to calculate the ground state wavefunction
+  ! for a particle in a harmonic potential, V=(x**2)/2,
+  ! by solving the corresponding diffusion equation in imaginary time.
 
-  REAL, PARAMETER :: pi = 4.0 * ATAN ( 1.0 ), const = (1.0 / pi) ** (1.0/4.0)
+  ! In atomic units, mass=1, hbar=1, diffusion coefficient D = hbar**2/(2*m) = 1/2,
+  ! so root-mean-squared displacement in time step ds is sqrt(2*D*ds) = sqrt(ds)
+  ! Walkers are created and destroyed, depending on the potential energy relative to the trial energy et
+  ! The program adjusts et to attempt to converge on a target number of walkers
+  ! hence obtaining the exact ground state value et = 0.5
+  ! This type of simulation is very sensitive to the initial guess for et,
+  ! and the resulting time evolution is quite noisy:
+  ! results are output as averages over the production period.
+  ! The simulated ground state wave function may be compared with the exact result for this simple problem.
 
-  INTEGER         :: n, n_max, n_target, nbin, nk
-  INTEGER         :: i, j, maxstep, step, count
-  INTEGER         :: nlive, ibin, bincount
-  REAL            :: lx, de, ds, var, et, resid, lx2, k
-  REAL            :: zeta, dx
-  REAL            :: factor, pexact, xbin
+  ! The default parameters run the simulation with et = 0.5, the exact groundstate energy for this potential.
+  ! You can then try, say, et = 0.6 and et = 0.4, observing the behaviour of the number of walkers in each case.
 
-  NAMELIST /params/ maxstep, lx, et, ds, dx, n_max, n_target
+  ! Many of the parameters, and the updating scheme for et, are taken from the following paper:
+  ! I Kostin, B Faber and K Schulten, Amer J Phys, 64, 633 (1996)
+
+  REAL,    DIMENSION(:), ALLOCATABLE  :: x       ! position of each walker (n_max)
+  REAL,    DIMENSION(:), ALLOCATABLE  :: v       ! potential energy of each walker (n_max)
+  INTEGER, DIMENSION(:), ALLOCATABLE  :: replica ! number of replicas to make (n_max)
+  LOGICAL, DIMENSION(:), ALLOCATABLE  :: alive   ! flags those walkers still alive (n_max)
+  INTEGER, DIMENSION(:), ALLOCATABLE  :: bin     ! histogram bins for wavefunction (n_bin)
+  REAL,    DIMENSION(:), ALLOCATABLE  :: psi     ! wavefunction (n_bin)
+
+  REAL, PARAMETER :: pi = 4.0 * ATAN ( 1.0 ), const = (1.0/pi) ** (1.0/4.0)
+
+  INTEGER :: n, n_max, n_target, n_add, n_bin, nlive, ibin
+  INTEGER :: i, production_steps, equilibration_steps, output_interval, step, step_count
+  REAL    :: x_max, de, ds, et, k, et_avg, pot_avg, pot
+  REAL    :: zeta, dx
+  REAL    :: factor, pexact, x_bin
+
+  NAMELIST /params/ production_steps, equilibration_steps, output_interval, x_max, et, ds, n_bin, n_max, n_target
 
   WRITE(*,'(''qmc_walk_sho'')')
-  WRITE(*,'(''diffusion Monte Carlo simulation of a quantum oscillator'')')
+  WRITE(*,'(''Diffusion Monte Carlo simulation of a quantum oscillator'')')
   WRITE(*,'(''Results in atomic units'')')
 
-  !set up parameters for the simulation
-
-  n_max = 1000 ! max number of walkers
-  n_target = 100 ! target number of walkers
-  maxstep = 40000                 ! number of steps
-  lx      = 8.0                   ! length of the line, centred at x = 0.0
-  et      = 0.5                   ! initial estimate of ground state energy in atomic units for spring contant k=1
-  ds      = 0.001                 ! timestep in imaginary time
-  dx      = 0.2                   ! bin interval for calculating the wavefunction
+  ! Set up default parameters for the simulation
+  n_max               = 2000  ! max number of walkers
+  n_target            = 500   ! target number of walkers
+  production_steps    = 20000 ! number of steps for production
+  equilibration_steps = 20000 ! number of steps for equilibration
+  output_interval     = 200   ! output interval
+  et                  = 0.5   ! initial estimate of ground state energy in atomic units
+  ds                  = 0.1   ! timestep in imaginary time
+  x_max               = 10.0  ! half length of the line, centred at x = 0.0 
+  n_bin               = 200   ! number of bins covering -x_max .. x_max
   READ(*,nml=params)
-  var     = SQRT(2.0 * diff * ds) ! the variance for gaussian random number
-  lx2     = lx / 2.0              ! line goes from -lx2 to lx2
-  n = n_target
-  IF ( n > n_max ) STOP 'n exceeds n_max at start'
-  nbin     = NINT(lx /dx)
+  WRITE(*,'(a,t40,i15)'  ) 'Max number of walkers = ',                   n_max
+  WRITE(*,'(a,t40,i15)'  ) 'Target number of walkers = ',                n_target
+  WRITE(*,'(a,t40,i15)'  ) 'Number of steps for production = ',          production_steps
+  WRITE(*,'(a,t40,i15)'  ) 'Number of steps for equilibration = ',       equilibration_steps
+  WRITE(*,'(a,t40,i15)'  ) 'Output interval = ',                         output_interval
+  WRITE(*,'(a,t40,f15.5)') 'Initial estimate of ground state energy = ', et
+  WRITE(*,'(a,t40,f15.5)') 'Time step = ',                               ds
+  WRITE(*,'(a,t40,f15.5)') 'Max x for wavefunction = ',                  x_max
+  WRITE(*,'(a,t40,i15)'  ) 'Number of bins for wavefunction = ',         n_bin
 
-  ALLOCATE ( x(n_max), replica(n_max) )
-  ALLOCATE ( psi(nbin), bin(nbin) )
+  dx = 2.0*x_max / REAL(n_bin)  ! Bin interval for calculating the wavefunction
+  IF ( n_target > n_max ) STOP 'n_target exceeds n_max at start'
+
+  ALLOCATE ( x(n_max), v(n_max), replica(n_max), alive(n_max) )
+  ALLOCATE ( psi(n_bin), bin(n_bin) )
 
   CALL RANDOM_SEED()
 
-  ! set up an initial delta distribution of walkers at origin
-  x = 0.0
+  ! Set up an initial delta distribution of walkers at origin
+  n = n_target
+  x(1:n) = 0.0
 
-  ! set up the array for calculating the wave function
+  ! Set up energy averages and array for calculating the wave function
+  bin(1:n_bin) = 0
+  step_count   = 0
+  et_avg       = 0.0
+  pot_avg      = 0.0
 
-  bincount    = 0
-  bin = 0
+  WRITE(*,'(''           step       nwalkers             e0            <v>'')')
 
+  DO step = 1, equilibration_steps+production_steps  ! Loop over steps
 
-  ! loop over steps
-
-  WRITE(*,'(''    step   nwalkers        e0'')')
-
-  DO step = 1, maxstep
-
-     ! Calculate the replica number for each walker
-
+     ! Move each walker forward by ds in imaginary time
      DO i = 1, n
-
-        x(i) = x(i) + random_normal( 0.0, var ) ! move walker forward by ds in imaginary time
-        de   = (0.5 * x(i) * x(i) - et) *  ds   ! calculate the scaled energy difference with the trial ground state
-
-        IF ( de  < 0.0 ) THEN                         ! k greater than 1
-           k = EXP( -de )
-           nk = FLOOR( k )
-           resid = k - REAL( nk )
-           CALL RANDOM_NUMBER( zeta )
-           IF ( resid >= zeta ) THEN
-              replica(i) = MIN ( nk + 1, 3 ) ! following Kozstin et al (1996)
-           ELSE
-              replica(i) = MIN ( nk, 3 ) ! following Kozstin et al (1996)
-           ENDIF
-        ELSE                                    ! k less than 1
-           k = EXP( -de )
-           resid = 1.0 - k
-           CALL RANDOM_NUMBER( zeta )
-           IF( resid >= zeta ) THEN
-              replica(i) = 0
-           ELSE
-              replica(i) = 1
-           ENDIF
-        ENDIF
-
-     ENDDO
-
-     ! remove walkers with replica=0
-     nlive            = COUNT( replica(1:n) > 0 )
-     x(1:nlive)       = PACK ( x(1:n),       replica(1:n) > 0 )
-     replica(1:nlive) = PACK ( replica(1:n), replica(1:n) > 0 )
-
-     ! expand the array x according to the replica number
-
-     n  = nlive
-
-     DO i = 1, nlive
-
-        IF ( replica(i) > 1 ) THEN ! only if more replicas are needed
-           DO j = 1, replica(i) - 1 
-              n = n + 1
-              IF ( n > n_max ) STOP 'n exceeds n_max in run'
-              x(n) = x(i)
-           END DO
-        END IF
-
+        x(i) = x(i) + random_normal( 0.0, SQRT(ds) ) ! Brownian dynamics step
      END DO
 
+     ! Calculate potential energy of each walker
+     v(1:n) = 0.5 * x(1:n)**2
+
+     ! Calculate the replica number for each walker
+     DO i = 1, n
+        de = ( v(i) - et ) *  ds           ! scaled energy difference with the trial ground state
+        k  = EXP( -de )                    ! new number of replicas
+        CALL RANDOM_NUMBER( zeta )
+        replica(i) = FLOOR ( k+zeta )      ! integerized number of replicas
+        replica(i) = MIN ( replica(i), 3 ) ! guard against too rapid growth
+     END DO
+
+     ! Remove walkers with replica = 0
+     alive(1:n)       = replica(1:n) > 0
+     nlive            = COUNT( alive(1:n) )
+     x(1:nlive)       = PACK ( x(1:n),       alive(1:n) )
+     v(1:nlive)       = PACK ( v(1:n),       alive(1:n) )
+     replica(1:nlive) = PACK ( replica(1:n), alive(1:n) )
+
+     ! Replicate walkers with replica > 1
+     n  = nlive
+     DO i = 1, nlive
+        n_add = replica(i) - 1
+        IF ( n_add > 0 ) THEN
+           IF ( n + n_add > n_max ) STOP 'n exceeds n_max in run'
+           x(n+1:n+n_add) = x(i)
+           v(n+1:n+n_add) = v(i)
+           n = n + n_add
+        END IF
+     END DO
+
+     pot = SUM(v(1:n))/REAL(n) ! potential averaged over walkers
+
      ! Stop if n is too large or too small
-
-     IF( n < 3 ) THEN
+     IF ( n < 3 ) THEN
         STOP 'n is too small, increase the value of et'
-     ELSEIF(n > (n_max-3) ) THEN
+     ELSE IF (n > (n_max-3) ) THEN
         STOP 'n is too large, decrease the value of et'
+     END IF
+
+     ! Periodically write the current number of walkers, trial energy, average potential
+
+     IF( MOD(step,output_interval) == 0 ) THEN
+        WRITE(*,'(i15,i15,2f15.5)') step, n, et, pot
      ENDIF
 
-     ! Periodically write the current number of walkers
+     ! Calculate the distribution of walkers on the line and energy averages
 
-     IF( MOD(step,50) == 0 ) THEN
-        WRITE(*,'(i7,i9,f16.6)') step, n, et
-     ENDIF
-
-     ! Periodically calculate the distribution of walkers on the line
-
-     IF( MOD(step, 5 ) == 0 ) THEN
+     IF ( step > equilibration_steps ) THEN
 
         DO i = 1, n
-
-           ibin = INT( (x(i)+lx2)/dx ) + 1
-           IF ( ibin >= 0 .AND. ibin <= nbin ) THEN
+           ibin = INT( (x(i)+x_max)/dx ) + 1
+           IF ( ibin >= 1 .AND. ibin <= n_bin ) THEN
               bin(ibin) = bin(ibin) +  1
            END IF
-        ENDDO
-        bincount = bincount + n
-     ENDIF
+        END DO
 
-     ! reset trial energy
-     et = 0.5 * SUM ( x(1:n)**2)/REAL(n) + 0.001*(1.0-REAL(n)/REAL(n_target)) ! following Kozstin et al (1996)
-     
-  ENDDO ! end over steps
+        et_avg     = et_avg     + et
+        pot_avg    = pot_avg    + pot
+        step_count = step_count + 1
 
-  ! normalize the wavefunction so that the integral over psi**2 = 1.0
-  psi    =  bin * lx  / REAL( bincount) / dx
-  factor = SUM ( psi**2 )
-  factor = SQRT( factor * dx )
-  psi    = psi / factor
+     END IF
 
-  ! print ground state wave function
-  WRITE(*,'(/)')
-  WRITE(*, '(''     x            psi(x)    psi_exact(x)'')')
-  
-  DO i = 1, nbin
+     ! Reset trial energy following Kozstin et al (1996)
+     et = pot + (1.0-REAL(n)/REAL(n_target))/ds
 
-     xbin    = -lx2 + ( REAL(i - 1) + 0.5 ) * dx
-     pexact  = const * EXP(- 0.5 * xbin * xbin)
-     WRITE(*,'(3f12.6)') xbin, psi(i), pexact
+  ENDDO ! End loop over steps
 
+  ! Normalize the wavefunction so that the integral over psi**2 = 1.0
+  psi     = REAL ( bin )         ! un-normalized wave function
+  factor  = SUM ( psi**2 ) * dx  ! integral, assuming that -x_max .. +x_max catches everything
+  psi     = psi / SQRT( factor ) ! normalizing factor
+  et_avg  = et_avg / REAL(step_count)
+  pot_avg = pot_avg / REAL(step_count)
+
+  WRITE(*,'(a,t40,f15.5)') 'Average trial energy = ',     et_avg
+  WRITE(*,'(a,t40,f15.5)') 'Average potential energy = ', pot_avg
+
+  ! Print ground state wave function
+  WRITE(*, '(''              x         psi(x)   psi_exact(x)'')')
+
+  DO i = 1, n_bin
+     x_bin  = -x_max + ( REAL(i - 1) + 0.5 ) * dx
+     pexact = const * EXP(- 0.5 * x_bin * x_bin)
+     WRITE(*,'(3f15.5)') x_bin, psi(i), pexact
   END DO
 
-  DEALLOCATE ( x, replica, bin, psi )
+  DEALLOCATE ( x, v, replica, alive, bin, psi )
 
 END PROGRAM qmc_walk_sho
 
