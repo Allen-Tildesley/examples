ab6c81fe5e61e5abb954814fd1fdb4b37dcc5292
Mike <m.p.allen@warwick.ac.uk>
2016-09-13 13:20:12 +0100

    first stab at SMC program

--- smc_lj_module.f90
+++ smc_lj_module.f90
@@ -1,25 +1,27 @@
 ! smc_lj_module.f90
 ! Energy, force, and move routines for SMC, LJ potential
 MODULE smc_module
-
-  ! TODO (MPA): adapt this to SMC
   
   USE, INTRINSIC :: iso_fortran_env, ONLY : error_unit
 
   IMPLICIT NONE
   PRIVATE
 
-  PUBLIC :: n, r, v, f
+  PUBLIC :: n, r, r_old, v, f, f1, move, lt, ne, gt
   PUBLIC :: model_description, allocate_arrays, deallocate_arrays
-  public :: force, energy_lrc
+  public :: force, force1, energy_lrc
 
-  INTEGER                              :: n ! number of atoms
-  REAL,    DIMENSION(:,:), ALLOCATABLE :: r ! positions (3,n)
-  REAL,    DIMENSION(:,:), ALLOCATABLE :: v ! velocities (3,n)
-  REAL,    DIMENSION(:,:), ALLOCATABLE :: f ! forces (3,n)
+  INTEGER                              :: n     ! number of atoms
+  REAL,    DIMENSION(:,:), ALLOCATABLE :: r     ! positions (3,n)
+  REAL,    DIMENSION(:,:), ALLOCATABLE :: r_old ! old positions (3,n)
+  REAL,    DIMENSION(:,:), ALLOCATABLE :: v     ! velocities (3,n)
+  REAL,    DIMENSION(:,:), ALLOCATABLE :: f     ! forces (3,n)
+  REAL,    DIMENSION(:,:), ALLOCATABLE :: f1    ! forces (3,n) used in force1
+  logical, dimension(:,:), allocatable :: move  ! mask for multi-atom moves
 
-  REAL, PARAMETER :: sigma = 1.0 ! LJ diameter (unit of length)
-  REAL, PARAMETER :: epslj = 1.0 ! LJ well depth (unit of energy)
+  INTEGER, PARAMETER :: lt = -1, ne = 0, gt = 1 ! j-range options
+  REAL,    PARAMETER :: sigma = 1.0 ! LJ diameter (unit of length)
+  REAL,    PARAMETER :: epslj = 1.0 ! LJ well depth (unit of energy)
 
 CONTAINS
 
@@ -37,7 +39,7 @@ CONTAINS
 
     REAL :: r_cut_box
 
-    ALLOCATE ( r(3,n), v(3,n), f(3,n) )
+    ALLOCATE ( r(3,n), r_old(3,n), v(3,n), f(3,n), f1(3,n), move(3,n) )
 
     r_cut_box = r_cut / box
     IF ( r_cut_box > 0.5 ) THEN
@@ -48,7 +50,7 @@ CONTAINS
   END SUBROUTINE allocate_arrays
 
   SUBROUTINE deallocate_arrays
-    DEALLOCATE ( r, v, f )
+    DEALLOCATE ( r, r_old, v, f, f1, move )
   END SUBROUTINE deallocate_arrays
 
   SUBROUTINE force ( box, r_cut, pot, pot_sh, vir )
@@ -59,10 +61,43 @@ CONTAINS
     REAL, INTENT(out) :: vir    ! virial
 
     ! Calculates potential (unshifted and shifted), virial and forces
+    ! Actual calculation is performed by subroutine energy_1
+    ! Forces are accumulated in f; contributions calculated by force1 are in f1
+
+    INTEGER            :: i
+    REAL               :: pot1, pot1_sh, vir1
+
+    f      = 0.0
+    pot    = 0.0
+    pot_sh = 0.0
+    vir    = 0.0
+
+    DO i = 1, n - 1 ! Begin loop over atoms
+
+       call force1 ( box, r_cut, i, gt, pot1, pot1_sh, vir1 )
+       f      = f + f1
+       pot    = pot + pot1
+       pot_sh = pot_sh + pot1_sh
+       vir    = vir + vir1
+      
+    END DO ! End loop over atoms
+
+  END SUBROUTINE force
+
+  SUBROUTINE force1 ( box, r_cut, i, j_range, pot, pot_sh, vir )
+    REAL,    INTENT(in)  :: box     ! simulation box length
+    REAL,    INTENT(in)  :: r_cut   ! potential cutoff distance
+    integer, intent(in)  :: i       ! index of atom of interest
+    integer, intent(in)  :: j_range ! index range of partner atoms
+    REAL,    INTENT(out) :: pot     ! total potential energy
+    REAL,    INTENT(out) :: pot_sh  ! potential shifted to zero at cutoff
+    REAL,    INTENT(out) :: vir     ! virial
+
+    ! Calculates potential (unshifted and shifted), virial and forces f1
     ! It is assumed that positions are in units where box = 1
     ! Forces are calculated in units where sigma = 1 and epsilon = 1
 
-    INTEGER            :: i, j, n_cut
+    INTEGER            :: j, j1, j2, n_cut
     REAL               :: r_cut_box, r_cut_box_sq, box_sq, rij_sq, sr2, sr6, sr12, potij, virij
     REAL, DIMENSION(3) :: rij, fij
 
@@ -70,40 +105,50 @@ CONTAINS
     r_cut_box_sq = r_cut_box ** 2
     box_sq       = box ** 2
 
-    f     = 0.0
+    f1    = 0.0
     pot   = 0.0
     vir   = 0.0
     n_cut = 0
 
-    DO i = 1, n - 1 ! Begin outer loop over atoms
+    SELECT CASE ( j_range )
+    CASE ( lt ) ! j < i
+       j1 = 1
+       j2 = i-1
+    CASE ( gt ) ! j > i
+       j1 = i+1
+       j2 = n
+    CASE ( ne ) ! j /= i
+       j1 = 1
+       j2 = n
+    END SELECT
 
-       DO j = i + 1, n ! Begin inner loop over atoms
+       DO j = j1, j2 ! Begin loop over atoms
 
+          if ( j == i ) cycle
+          
           rij(:) = r(:,i) - r(:,j)           ! separation vector
           rij(:) = rij(:) - ANINT ( rij(:) ) ! periodic boundary conditions in box=1 units
           rij_sq = SUM ( rij**2 )            ! squared separation
 
           IF ( rij_sq < r_cut_box_sq ) THEN ! check within cutoff
 
-             rij_sq = rij_sq * box_sq ! now in sigma=1 units
-             rij(:) = rij(:) * box    ! now in sigma=1 units
-             sr2    = 1.0 / rij_sq
-             sr6    = sr2 ** 3
-             sr12   = sr6 ** 2
-             potij  = sr12 - sr6
-             virij  = potij + sr12
-             pot    = pot + potij
-             vir    = vir + virij
-             fij    = rij * virij / rij_sq
-             f(:,i) = f(:,i) + fij
-             f(:,j) = f(:,j) - fij
-             n_cut  = n_cut + 1
+             rij_sq  = rij_sq * box_sq ! now in sigma=1 units
+             rij(:)  = rij(:) * box    ! now in sigma=1 units
+             sr2     = 1.0 / rij_sq
+             sr6     = sr2 ** 3
+             sr12    = sr6 ** 2
+             potij   = sr12 - sr6
+             virij   = potij + sr12
+             pot     = pot + potij
+             vir     = vir + virij
+             fij     = rij * virij / rij_sq
+             f1(:,i) = f1(:,i) + fij
+             f1(:,j) = f1(:,j) - fij
+             n_cut   = n_cut + 1
 
           END IF ! End check within cutoff
 
        END DO ! End inner loop over atoms
-
-    END DO ! End outer loop over atoms
 
     ! calculate shifted potential
     sr2    = 1.0 / r_cut**2 ! in sigma=1 units
@@ -113,12 +158,13 @@ CONTAINS
     pot_sh = pot - REAL ( n_cut ) * potij
 
     ! multiply results by numerical factors
-    f      = f      * 24.0
+    f1     = f1      * 24.0
     pot    = pot    * 4.0
     pot_sh = pot_sh * 4.0
     vir    = vir    * 24.0 / 3.0
 
-  END SUBROUTINE force
+  END SUBROUTINE force1
+
 
   SUBROUTINE energy_lrc ( n, box, r_cut, pot, vir )
     INTEGER, INTENT(in)  :: n        ! number of atoms
--- smc_nvt_lj.f90
+++ smc_nvt_lj.f90
@@ -2,23 +2,26 @@
 ! Smart Monte Carlo, NVT ensemble
 PROGRAM smc_nvt_lj
 
-  ! TODO (MPA) adapt this to SMC
-  
   USE, INTRINSIC :: iso_fortran_env, ONLY : input_unit, output_unit, error_unit, iostat_end, iostat_eor
 
   USE config_io_module, ONLY : read_cnf_atoms, write_cnf_atoms
   USE averages_module,  ONLY : time_stamp, run_begin, run_end, blk_begin, blk_end, blk_add
-  USE maths_module,     ONLY : random_normals
-  USE md_module,        ONLY : model_description, allocate_arrays, deallocate_arrays, &
-       &                       force, r, v, f, n, energy_lrc
+  USE maths_module,     ONLY : random_normals, metropolis
+  USE smc_module,       ONLY : model_description, allocate_arrays, deallocate_arrays, &
+       &                       force, force1, energy_lrc, &
+       &                       r, r_old, v, f, f1, move, n, ne
 
   IMPLICIT NONE
 
-  ! Takes in a configuration of atoms (positions, velocities)
+  ! Takes in a configuration of atoms (positions)
   ! Cubic periodic boundary conditions
-  ! Conducts molecular dynamics using BAOAB algorithm of BJ Leimkuhler and C Matthews
-  ! Appl. Math. Res. eXpress 2013, 34–56 (2013); J. Chem. Phys. 138, 174102 (2013)
+  ! Conducts Smart Monte Carlo using Hybrid Monte Carlo / Brownian Dynamics notation
   ! Uses no special neighbour lists
+  ! Assume that a sweep consists of either
+  ! (a) N successive single-particle moves
+  ! (b) 1 multi-particle move involving a large fraction of atoms
+  ! (large enough to justify calling the complete force routine)
+  ! The ensemble corresponds to the shifted potential, not the simple cutoff potential
 
   ! Reads several variables and options from standard input using a namelist nml
   ! Leave namelist empty to accept supplied defaults
@@ -32,32 +35,34 @@ PROGRAM smc_nvt_lj
   ! The model is defined in md_module
 
   ! Most important variables
-  REAL :: box         ! box length
-  REAL :: density     ! density
-  REAL :: dt          ! time step
-  REAL :: r_cut       ! potential cutoff distance
-  REAL :: pot         ! total potential energy
-  REAL :: pot_sh      ! total shifted potential energy
-  REAL :: kin         ! total kinetic energy
-  REAL :: vir         ! total virial
-  REAL :: pressure    ! pressure (to be averaged)
-  REAL :: temperature ! temperature (specified)
-  REAL :: energy      ! total energy per atom (to be averaged)
-  REAL :: energy_sh   ! total shifted energy per atom (to be averaged)
-  REAL :: gamma       ! friction coefficient
+  REAL    :: box         ! box length
+  REAL    :: density     ! density
+  REAL    :: dt          ! time step
+  REAL    :: r_cut       ! potential cutoff distance
+  REAL    :: pot         ! total potential energy
+  REAL    :: pot_sh      ! total shifted potential energy
+  REAL    :: vir         ! total virial
+  REAL    :: pressure    ! pressure (to be averaged)
+  REAL    :: temperature ! temperature (specified)
+  REAL    :: energy      ! total energy per atom (to be averaged)
+  REAL    :: energy_sh   ! total shifted energy per atom (to be averaged)
+  INTEGER :: move_mode   ! selects single- or multi-atom moves
+  REAL    :: fraction    ! fraction of atoms to move in multi-atom move
 
   INTEGER :: blk, stp, nstep, nblock, ioerr
-  REAL    :: pot_lrc, vir_lrc, c0, c1
+  INTEGER :: i, n_move
+  REAL    :: pot_lrc, vir_lrc, pot_old, pot_new, pot_sh_old, pot_sh_new, vir_old, vir_new
+  REAL    :: kin_old, kin_new, delta, move_ratio
 
   CHARACTER(len=4), PARAMETER :: cnf_prefix = 'cnf.'
   CHARACTER(len=3), PARAMETER :: inp_tag = 'inp', out_tag = 'out'
   CHARACTER(len=3)            :: sav_tag = 'sav' ! may be overwritten with block number
+  INTEGER,          PARAMETER :: single_atom = 1, multi_atom = 2
 
-  NAMELIST /nml/ nblock, nstep, r_cut, dt, gamma, temperature
+  NAMELIST /nml/ nblock, nstep, r_cut, dt, move_mode, temperature, fraction
 
-  WRITE ( unit=output_unit, fmt='(a)' ) 'bd_nvt_lj'
-  WRITE ( unit=output_unit, fmt='(a)' ) 'Brownian dynamics, constant-NVT ensemble'
-  WRITE ( unit=output_unit, fmt='(a)' ) 'Particle mass=1 throughout'
+  WRITE ( unit=output_unit, fmt='(a)' ) 'smc_nvt_lj'
+  WRITE ( unit=output_unit, fmt='(a)' ) 'Smart Monte Carlo, constant-NVT ensemble'
   CALL model_description ( output_unit )
   CALL time_stamp ( output_unit )
 
@@ -65,36 +70,36 @@ PROGRAM smc_nvt_lj
   nblock      = 10
   nstep       = 1000
   r_cut       = 2.5
-  dt          = 0.005
-  gamma       = 0.1
+  dt          = 0.05
   temperature = 0.7
+  move_mode   = 1
+  fraction    = 0.5
 
   READ ( unit=input_unit, nml=nml, iostat=ioerr )
   IF ( ioerr /= 0 ) THEN
      WRITE ( unit=error_unit, fmt='(a,i15)') 'Error reading namelist nml from standard input', ioerr
      IF ( ioerr == iostat_eor ) WRITE ( unit=error_unit, fmt='(a)') 'End of record'
      IF ( ioerr == iostat_end ) WRITE ( unit=error_unit, fmt='(a)') 'End of file'
-     STOP 'Error in bd_nvt_lj'
+     STOP 'Error in smc_nvt_lj'
   END IF
   WRITE ( unit=output_unit, fmt='(a,t40,i15)'   ) 'Number of blocks',          nblock
   WRITE ( unit=output_unit, fmt='(a,t40,i15)'   ) 'Number of steps per block', nstep
   WRITE ( unit=output_unit, fmt='(a,t40,f15.5)' ) 'Potential cutoff distance', r_cut
   WRITE ( unit=output_unit, fmt='(a,t40,f15.5)' ) 'Time step',                 dt
-  WRITE ( unit=output_unit, fmt='(a,t40,f15.5)' ) 'Friction coefficient',      gamma
   WRITE ( unit=output_unit, fmt='(a,t40,f15.5)' ) 'Temperature',               temperature
-  WRITE ( unit=output_unit, fmt='(a,t40,f15.5)' ) 'Ideal diffusion coefft',    temperature / gamma
-
-  ! Dimensionless coefficients for random momentum term
-  gamma = gamma * dt ! incorporate timestep into friction
-  c0 = EXP(-gamma)
-  IF ( gamma > 1.e-5 ) THEN
-     c1 = 1-c0**2
+  IF ( move_mode == single_atom ) THEN
+     WRITE ( unit=output_unit, fmt='(a,t40,a)' ) 'Move mode is ', 'single-atom'
+  ELSE IF ( move_mode == multi_atom ) THEN
+     WRITE ( unit=output_unit, fmt='(a,t40,a)'     ) 'Move mode is ', 'multi-atom'
+     WRITE ( unit=output_unit, fmt='(a,t40,f15.5)' ) 'Fraction of atoms moving', fraction
+     IF ( fraction < 0.0 .OR. fraction > 1.0 ) THEN
+        WRITE ( unit=error_unit, fmt='(a)') 'Error: fraction out of range'
+        STOP 'Error in smc_nvt_lj'
+     END IF
   ELSE
-     c1 = gamma * ( 2.0 - gamma * ( 2.0 - 4.0 * gamma / 3.0 ) ) ! Taylor expansion for low gamma
+     WRITE ( unit=error_unit, fmt='(a,i15)') 'Error: move_mode out of range', move_mode
+     STOP 'Error in smc_nvt_lj'
   END IF
-  c1 = SQRT ( c1 )
-  WRITE ( unit=output_unit, fmt='(a,t40,es15.4)' ) 'Algorithm coefficient c0', c0
-  WRITE ( unit=output_unit, fmt='(a,t40,es15.4)' ) 'Algorithm coefficient c1', c1
 
   CALL read_cnf_atoms ( cnf_prefix//inp_tag, n, box ) ! First call is just to get n and box
   WRITE ( unit=output_unit, fmt='(a,t40,i15)'   ) 'Number of particles',  n
@@ -104,7 +109,7 @@ PROGRAM smc_nvt_lj
 
   CALL allocate_arrays ( box, r_cut )
 
-  CALL read_cnf_atoms ( cnf_prefix//inp_tag, n, box, r, v ) ! Second call gets r and v
+  CALL read_cnf_atoms ( cnf_prefix//inp_tag, n, box, r ) ! Second call gets r
 
   r(:,:) = r(:,:) / box              ! Convert positions to box units
   r(:,:) = r(:,:) - ANINT ( r(:,:) ) ! Periodic boundaries
@@ -113,17 +118,15 @@ PROGRAM smc_nvt_lj
   CALL energy_lrc ( n, box, r_cut, pot_lrc, vir_lrc )
   pot         = pot + pot_lrc
   vir         = vir + vir_lrc
-  kin         = 0.5*SUM(v**2)
-  energy      = ( pot + kin ) / REAL ( n )
-  energy_sh   = ( pot_sh + kin ) / REAL ( n )
-  temperature = 2.0 * kin / REAL ( 3*(n-1) )
+  energy      = 1.5*temperature + pot / REAL ( n )
+  energy_sh   = 1.5*temperature + pot_sh / REAL ( n )
   pressure    = density * temperature + vir / box**3
   WRITE ( unit=output_unit, fmt='(a,t40,f15.5)' ) 'Initial energy',         energy
   WRITE ( unit=output_unit, fmt='(a,t40,f15.5)' ) 'Initial shifted energy', energy_sh
   WRITE ( unit=output_unit, fmt='(a,t40,f15.5)' ) 'Initial temperature',    temperature
   WRITE ( unit=output_unit, fmt='(a,t40,f15.5)' ) 'Initial pressure',       pressure
 
-  CALL run_begin ( [ CHARACTER(len=15) :: 'Energy', 'Shifted Energy', 'Temperature', 'Pressure' ] )
+  CALL run_begin ( [ CHARACTER(len=15) :: 'Move Ratio', 'Energy', 'Shifted Energy', 'Pressure' ] )
 
   DO blk = 1, nblock ! Begin loop over blocks
 
@@ -131,33 +134,84 @@ PROGRAM smc_nvt_lj
 
      DO stp = 1, nstep ! Begin loop over steps
 
-        ! BAOAB algorithm
-        v(:,:) = v(:,:) + 0.5 * dt * f(:,:)               ! Kick half-step (B)
-        r(:,:) = r(:,:) + 0.5 * dt * v(:,:) / box         ! Drift half-step (A) (positions in box=1 units)
-        CALL random_normals ( 0.0, SQRT(temperature), f ) ! Use f to hold random momenta
-        v(:,:) = c0 * v(:,:) + c1 * f                     ! Friction and random effects (O)
-        r(:,:) = r(:,:) + 0.5 * dt * v(:,:) / box         ! Drift half-step (A) (positions in box=1 units)
-        r(:,:) = r(:,:) - ANINT ( r(:,:) )                ! Periodic boundaries (box=1 units)
-        CALL force ( box, r_cut, pot, pot_sh, vir )       ! Force evaluation
-        v(:,:) = v(:,:) + 0.5 * dt * f(:,:)               ! Kick half-step (B)
+        SELECT CASE ( move_mode )
+
+        CASE ( single_atom )
+
+           n_move = 0
+           DO i = 1, n ! loop over atoms
+              r_old(:,i) = r(:,i)! Store old position of this atom
+              CALL random_normals ( 0.0, SQRT(temperature), v(:,i) )          ! choose random momentum
+              kin_old = 0.5*SUM(v(:,i)**2)                                    ! Compute old kinetic energy of this atom
+              CALL force1 ( box, r_cut, i, ne, pot_old, pot_sh_old, vir_old ) ! old force f1 and potential etc
+              v(:,i) = v(:,i) + 0.5 * dt * f1(:,i)                            ! Kick half-step for one atom
+              r(:,i) = r(:,i) + dt * v(:,i) / box                             ! Drift step (positions in box=1 units)
+              r(:,i) = r(:,i) - ANINT ( r(:,i) )                              ! Periodic boundaries (box=1 units)
+              CALL force1 ( box, r_cut, i, ne, pot_new, pot_sh_new, vir_new ) ! new force f1 and potential etc
+              v(:,i) = v(:,i) + 0.5 * dt * f1(:,i)                            ! Kick half-step for one atom
+              kin_new = 0.5*SUM(v(:,i)**2)                                    ! Compute new kinetic energy of this atom
+
+              delta = ( pot_sh_new - pot_sh_old + kin_new - kin_old ) / temperature
+              IF ( metropolis ( delta ) ) THEN ! accept Metropolis test
+                 pot    = pot + pot_new - pot_old          ! update potential energy
+                 vir    = vir + vir_new - vir_old          ! update virial
+                 pot_sh = pot_sh + pot_sh_new - pot_sh_old ! update shifted potential energy
+                 n_move = n_move + 1                       ! update move counter
+              ELSE
+                 r(:,i) = r_old(:,i) ! restore position
+              END IF ! reject Metropolis test
+
+           END DO ! end loop over atoms
+           move_ratio = REAL(n_move) / REAL(n)
+
+        CASE ( multi_atom )
+
+           CALL RANDOM_NUMBER ( v(1,:) )                         ! Select n uniform random numbers
+           move = SPREAD ( v(1,:) < fraction, dim=2, ncopies=3 ) ! Construct mask for moving atoms
+
+           r_old = r                                         ! Store old positions
+           CALL random_normals ( 0.0, SQRT(temperature), v ) ! Choose random momenta
+           kin_old    = 0.5*SUM(v**2,mask=move)              ! Compute old kinetic energy of moving atoms
+           pot_old    = pot                                  ! Store old total potential energy
+           vir_old    = vir                                  ! Store old total virial
+           pot_sh_old = pot_sh                               ! Store old total shifted potential energy
+           WHERE ( move )
+              v = v + 0.5 * dt * f ! Kick half-step
+              r = r + dt * v / box ! Drift step (positions in box=1 units)
+              r = r - ANINT ( r )  ! Periodic boundaries (box=1 units)
+           END WHERE
+           CALL force ( box, r_cut, pot, pot_sh, vir ) ! Force evaluation, new total potential energy etc
+           WHERE ( move ) v = v + 0.5 * dt * f         ! Kick half-step
+           kin_new = 0.5*SUM(v**2,mask=move)           ! Compute new kinetic energy of moving atoms
+
+           delta = ( pot_sh_new - pot_sh_old + kin_new - kin_old ) / temperature
+           IF ( metropolis ( delta ) ) THEN  ! accept Metropolis test
+              move_ratio = 1.0        ! set move counter
+           ELSE
+              r          = r_old      ! restore positions
+              pot        = pot_old    ! restore potential energy
+              vir        = vir_old    ! restore virial
+              pot_sh     = pot_sh_old ! restore shifted potential energy
+              move_ratio = 0.0        ! set move counter
+           END IF ! reject Metropolis test
+
+        END SELECT
 
         CALL energy_lrc ( n, box, r_cut, pot_lrc, vir_lrc )
-        pot         = pot + pot_lrc
-        vir         = vir + vir_lrc
-        kin         = 0.5*SUM(v**2)
-        energy      = ( pot + kin ) / REAL ( n )
-        energy_sh   = ( pot_sh + kin ) / REAL ( n )
-        temperature = 2.0 * kin / REAL ( 3*(n-1) )
-        pressure    = density * temperature + vir / box**3
+        pot       = pot + pot_lrc
+        vir       = vir + vir_lrc
+        energy    = 1.5*temperature + pot / REAL ( n )
+        energy_sh = 1.5*temperature + pot_sh / REAL ( n )
+        pressure  = density * temperature + vir / box**3
 
         ! Calculate all variables for this step
-        CALL blk_add ( [energy,energy_sh,temperature,pressure] )
+        CALL blk_add ( [move_ratio,energy,energy_sh,pressure] )
 
      END DO ! End loop over steps
 
      CALL blk_end ( blk, output_unit )
      IF ( nblock < 1000 ) WRITE(sav_tag,'(i3.3)') blk               ! number configuration by block
-     CALL write_cnf_atoms ( cnf_prefix//sav_tag, n, box, r*box, v ) ! save configuration
+     CALL write_cnf_atoms ( cnf_prefix//sav_tag, n, box, r*box ) ! save configuration
 
   END DO ! End loop over blocks
 
@@ -167,18 +221,15 @@ PROGRAM smc_nvt_lj
   CALL energy_lrc ( n, box, r_cut, pot_lrc, vir_lrc )
   pot         = pot + pot_lrc
   vir         = vir + vir_lrc
-  kin         = 0.5*SUM(v**2)
-  energy      = ( pot + kin ) / REAL ( n )
-  energy_sh   = ( pot_sh + kin ) / REAL ( n )
-  temperature = 2.0 * kin / REAL ( 3*(n-1) )
+  energy      = 1.5*temperature + pot / REAL ( n )
+  energy_sh   = 1.5*temperature + pot_sh / REAL ( n )
   pressure    = density * temperature + vir / box**3
   WRITE ( unit=output_unit, fmt='(a,t40,f15.5)' ) 'Final energy',         energy
   WRITE ( unit=output_unit, fmt='(a,t40,f15.5)' ) 'Final shifted energy', energy_sh
-  WRITE ( unit=output_unit, fmt='(a,t40,f15.5)' ) 'Final temperature',    temperature
   WRITE ( unit=output_unit, fmt='(a,t40,f15.5)' ) 'Final pressure',       pressure
   CALL time_stamp ( output_unit )
 
-  CALL write_cnf_atoms ( cnf_prefix//out_tag, n, box, r*box, v )
+  CALL write_cnf_atoms ( cnf_prefix//out_tag, n, box, r*box )
 
   CALL deallocate_arrays
 
