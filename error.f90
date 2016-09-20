be0cba5439549d671680c47f3e62f28dbdc7cfd2
LonelyProf <prof.mike.allen@gmail.com>
2016-09-20 17:56:08 +0100

    finished work on error.f90 for the moment

--- error.f90
+++ error.f90
@@ -1,8 +1,6 @@
 ! error.f90
 ! Estimated error in correlated data
 PROGRAM error
-  !
-  ! TODO MPA provide code
   USE, INTRINSIC :: iso_fortran_env, ONLY : input_unit, output_unit, error_unit, iostat_end, iostat_eor
 
   USE maths_module, ONLY : random_normal, init_random_seed
@@ -14,21 +12,21 @@ PROGRAM error
   ! see G Ciccotti and JP Ryckaert Mol Phys 40 141 (1980)
   ! and AD Baczewski and SD Bond J Chem Phys 139 044107 (2013)
 
-  REAL :: m     ! memory function coefficients
-  REAL :: kappa ! memory function decay rates
-  REAL :: theta ! auxiliary coefficient
-  REAL :: alpha ! auxiliary coefficient
-  REAL :: zeta  ! random number
-  REAL :: s     ! GLE auxiliary variable
-  REAL :: delta ! time step
-  REAL :: vt    ! velocity at time t
+  REAL    :: m      ! memory function coefficients
+  REAL    :: kappa  ! memory function decay rates
+  REAL    :: theta  ! auxiliary coefficient
+  REAL    :: alpha  ! auxiliary coefficient
+  REAL    :: zeta   ! random number
+  REAL    :: s      ! GLE auxiliary variable
+  REAL    :: delta  ! time step
+  REAL    :: at     ! dynamical variable at time t
+  INTEGER :: nstep  ! number of timesteps in run
+  INTEGER :: nequil ! number of equilibration timesteps
+  INTEGER :: t      ! time (equivalent to step number in file)
 
-  REAL, DIMENSION(:), ALLOCATABLE :: a     ! stored data values (nstep)
+  REAL, DIMENSION(:), ALLOCATABLE :: a ! stored data values (nstep)
 
-  INTEGER :: nstep           ! number of timesteps in run
-  INTEGER :: nequil          ! number of equilibration timesteps
-  INTEGER :: t               ! time (equivalent to step number in file)
-  INTEGER :: ioerr, tblock, i_repeat, nblock, blk, stp1, stp2
+  INTEGER :: ioerr, tblock, i_repeat, nblock, blk, stp1, stp2, trun
   REAL    :: average, variance, stddev, err, si, tcor, a_val, a_avg, a_var, a_var_1, a_err
 
   INTEGER, PARAMETER :: n_repeat = 50 ! number of simulation repeats for brute force empirical calculation
@@ -36,11 +34,11 @@ PROGRAM error
   NAMELIST /nml/ nstep, nequil, delta, variance, average
 
   ! Example default values
-  nstep           = 2**16 ! number of steps, about 60,000 for example
-  nequil          = 10000 ! number of equilibration timesteps
-  delta           = 0.01  ! timestep for simulation
-  variance        = 1.0   ! desired variance of data (equivalent to temperature)
-  average         = 1.0   ! desired average value of data
+  nstep    = 2**16 ! number of steps, about 60,000 for example
+  nequil   = 10000 ! number of equilibration timesteps
+  delta    = 0.01  ! timestep for simulation
+  variance = 1.0   ! desired variance of data (equivalent to temperature)
+  average  = 1.0   ! desired average value of data
 
   ! Namelist from standard input
   READ ( unit=input_unit, nml=nml, iostat=ioerr )
@@ -80,7 +78,7 @@ PROGRAM error
   ! The time correlation function is known, and hence the statistical inefficiency (SI)
   ! From this, the error in the mean for a run of any length can be calculated
 
-  tcor = 1.0/m        ! Correlation time is determined by memory function
+  tcor = 1.0 / m      ! Correlation time is determined by memory function
   tcor = tcor / delta ! express in timesteps
   si   = 2*tcor       ! Statistical inefficiency (SI)
   err  = SQRT(si*variance/REAL(nstep))
@@ -95,22 +93,22 @@ PROGRAM error
   ! For comparison, we do n_repeat independent runs and estimate the error in run averages directly from these
   ! This is to give an empirical idea of the distribution from which the run average is sampled
   ! Of course, we expect the results to be similar to the exact values just calculated
-  
+
   a_avg = 0.0 ! zero average accumulator
   a_var = 0.0 ! zero mean-squared accumulator
 
   DO i_repeat = 1, n_repeat ! loop over repeats of simulation
 
      ! Initial values
-     vt = 0.0
+     at = 0.0
      s  = 0.0
 
      DO t = -nequil, nstep ! include an equilibration period
-        vt = vt + 0.5*delta*s
+        at   = at + 0.5*delta*s
         zeta = random_normal ( 0.0, stddev )
-        s  = theta*s - (1.0-theta)*m*vt + alpha*SQRT(m)*zeta
-        vt = vt + 0.5*delta*s
-        IF ( t >= 1 ) a(t) = average + vt ! store values (adding average)
+        s    = theta*s - (1.0-theta)*m*at + alpha*SQRT(m)*zeta
+        at   = at + 0.5*delta*s
+        IF ( t >= 1 ) a(t) = average + at ! store values (adding average)
      END DO
 
      a_val = SUM(a) / REAL(nstep) ! the run average
@@ -128,9 +126,8 @@ PROGRAM error
   WRITE ( unit=output_unit, fmt='(a)') 'This should be in reasonable agreement with exact error estimate'
 
   ! Now analyse the last run, as if it were the only one that had been carried out
-  ! This is what usually happens in a simulation; we rarely have the luxury of many independent runs
-  ! We must take account of the correlations between successive values in time
-  
+  ! This is what usually happens; we rarely have the luxury of many independent runs
+
   ! Simple calculation of average and variance
   a_avg   = SUM ( a ) / REAL(nstep)      ! sample average
   a       = a - a_avg                    ! centre the data
@@ -143,56 +140,71 @@ PROGRAM error
   WRITE ( unit=output_unit, fmt='(a,t40,f15.5)' ) 'Error estimate neglecting correlations = ', a_err
   WRITE ( unit=output_unit, fmt='(a)') 'This should be very over-optimistic!'
 
-  ! The two common methods which follow are actually very similar; they differ in the choice of block lengths
+  ! We must take account of the correlations between successive values in time
+  ! The two common methods which follow are actually very similar
+  ! They differ in the choice of block lengths
 
   ! Traditional block analysis
   ! The rationale here is that 20 (say) independent block averages should be enough to get a reasonable
-  ! estimate of the desired quantities, and that there is not a lot to gain by looking at more/shorter blocks.
-  ! Indeed, some workers just assume that their run may be divided into 10 or 20 blocks, which they hope
-  ! will be independent, but we cannot recommend this, unless there is good reason to support the assumption.
-  ! Notwithstanding that warning, this is exactly what we do in our example programs, just for simplicity.
-  ! If the 20 blocks are not independent, then attention should be focused on fewer/longer blocks, rather than 
-  ! more/shorter ones, and a plot of squared error estimate, or SI, vs 1/tblock carried out to extrapolate to tblock=nstep.
-  ! The loop below provides the data for that plot.
-  ! Advocates of Flyvbjerg and Petersen might argue that the additional data points at low nblock / high tblock
-  ! do not actually provide much new (independent) information.
-  
+  ! estimate of the desired quantities, and that there is not a lot to gain by looking at more (shorter) blocks.
+  ! Some workers just assume that the run may be divided into 10 or 20 blocks, which they hope will be independent.
+  ! This is exactly what we do in our example programs, just for simplicity.
+  ! We cannot recommend this in general, unless there is good reason to support the assumption of independence.
+  ! If the 20 blocks are not independent, then attention should be focused on fewer (longer) blocks,
+  ! rather than more (shorter) ones, and a plot of squared error estimate, or statistical inefficiency,
+  ! vs 1/tblock carried out to extrapolate to tblock=nstep. The loop below provides the data for that plot.
+
   WRITE ( unit=output_unit, fmt='(4a15)' ) 'tblock', 'nblock', 'error estimate', 'estimate of SI'
-  do nblock = 20, 4, -1 ! loop over number (and hence length) of blocks
-     tblock = nstep / nblock ! block length (rounded down)
-     a_avg = sum(a(1:nblock*tblock)) / real(nblock*tblock) ! average of data
-     a_var = 0.0
-     do blk = 1, nblock ! loop over blocks
-        stp1 = (blk-1)*tblock+1 ! start of block
-        stp2 = blk*tblock       ! end of block
-        a_val = sum ( a(stp1:stp2) - a_avg ) / real(tblock) ! block average
-        a_var = a_var + a_val**2
-     end do ! end loop over blocks
-     a_var = a_var / real(nblock-1)    ! bias-corrected variance of block averages
-     a_err = sqrt(a_var/real(nblock))  ! estimate of error from block-average variance
-     si     = tblock*a_var/a_var_1         ! statistical inefficiency
+  DO nblock = 20, 4, -1 ! loop over number, and hence length, of blocks
+     tblock = nstep / nblock              ! block length in steps (rounded down)
+     trun   = nblock*tblock               ! run length in steps, accounting for rounding
+     a_avg  = SUM(a(1:trun)) / REAL(trun) ! average of data
+     a_var  = 0.0                         ! zero mean-square block average accumulator
+     DO blk = 1, nblock ! loop over blocks
+        stp1  = (blk-1)*tblock+1                            ! start of block
+        stp2  = blk*tblock                                  ! end of block
+        a_val = SUM ( a(stp1:stp2) - a_avg ) / REAL(tblock) ! block average
+        a_var = a_var + a_val**2                            ! mean-square block average
+     END DO ! end loop over blocks
+     a_var = a_var / REAL(nblock-1)    ! bias-corrected variance of block averages
+     a_err = SQRT(a_var/REAL(nblock))  ! estimate of error from block-average variance
+     si    = tblock * a_var / a_var_1  ! statistical inefficiency
      WRITE ( unit=output_unit, fmt='(2i15,3f15.6)' ) tblock, nblock, a_err, si
-  end do ! loop over number (and hence length) of blocks
+  END DO ! end loop over number, and hence length, of blocks
+  WRITE ( unit=output_unit, fmt='(a)' ) 'Plateau at large tblock (small nblock)'
+  WRITE ( unit=output_unit, fmt='(a)' ) 'should agree quite well with exact error estimate'
+  WRITE ( unit=output_unit, fmt='(a)' ) 'Can plot SI or error**2 against 1/tblock'
 
   ! Method of Flyvbjerg and Petersen, J Chem Phys, 91, 461 (1989)
   ! Note that, in this implementation, we over-write the data array a
+  ! Advantages of this method are the very neat method for reducing the data
+  ! and the formal scaling analysis that accompanies the blocking transformation
+  ! The basic calculation is the same as for the traditional block-averaging,
+  ! but the block length, and hence tblock, change by a factor 2 each time.
+  ! Advocates of Flyvbjerg and Petersen might argue that the additional data points
+  ! at low nblock (high tblock) which are calculated in the traditional method
+  ! do not actually provide much new (independent) information.
+  ! One may attempt an extrapolation of si or a_err**2 as a function of 1/tblock
+  ! but F&P suggest estimating a plateau in a plot vs number of blocking transformations
+  ! which is essentially log2(tblock)
   nblock = nstep
   tblock = 1
   WRITE ( unit=output_unit, fmt='(4a15)' ) 'tblock', 'nblock', 'error estimate', 'estimate of SI'
-  DO
+  DO ! loop over number, and hence length, of blocks
      nblock = nblock / 2 ! halve the number of blocks, rounding down if nblock is odd
      tblock = tblock*2   ! double the block length
      IF ( nblock < 3 ) EXIT
      a(1:nblock) = ( a(1:2*nblock-1:2) + a(2:2*nblock:2) ) / 2.0 ! blocking transformation, halving the data set
-     a_avg  = SUM(a(1:nblock))/REAL(nblock)      ! re-compute sample average
-     a(1:nblock) = a(1:nblock) - a_avg           ! re-centre in case of dropped points (odd nblock)
-     a_var  = SUM(a(1:nblock)**2)/REAL(nblock-1) ! bias-corrected variance of block averages
-     a_err  = SQRT(a_var/REAL(nblock))      ! estimate of error from block average variance
-     si     = tblock*a_var/a_var_1         ! statistical inefficiency
+     a_avg       = SUM ( a(1:nblock) ) / REAL(nblock)            ! re-compute sample average
+     a(1:nblock) = a(1:nblock) - a_avg                           ! re-centre in case of dropped points (odd nblock)
+     a_var       = SUM ( a(1:nblock)**2 ) / REAL(nblock-1)       ! bias-corrected variance of block averages
+     a_err       = SQRT ( a_var / REAL(nblock) )                 ! estimate of error from block average variance
+     si          = tblock * a_var / a_var_1                      ! statistical inefficiency
      WRITE ( unit=output_unit, fmt='(2i15,3f15.6)' ) tblock, nblock, a_err, si
-  END DO
-  WRITE ( unit=output_unit, fmt='(a)') 'Plateau at larger tblock / smaller nblock should agree quite well with exact error estimate'
-  WRITE ( unit=output_unit, fmt='(a)') 'Can plot against 1/tblock and extrapolate to tblock=infinity'
+  END DO ! end loop over number, and hence length, of blocks
+  WRITE ( unit=output_unit, fmt='(a)' ) 'Plateau at large tblock (small nblock)'
+  WRITE ( unit=output_unit, fmt='(a)' ) 'should agree quite well with exact error estimate'
+  WRITE ( unit=output_unit, fmt='(a)' ) 'Can plot SI or error**2 against 1/tblock'
 
 END PROGRAM error
 
