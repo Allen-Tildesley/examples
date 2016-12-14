065b7a2fa3ffbaee2465a04ec4a38fe297ec36f8
LonelyProf <prof.mike.allen@gmail.com>
2016-12-14 12:15:09 +0000

    updated README file

--- cc0.f90
+++ cc0.f90
@@ -1,5 +1,8 @@
-  ! Written in 2016 by Michael P Allen, m.p.allen@warwick.ac.uk, m.p.allen@bristol.ac.uk
-  ! and by Dominic J Tildesley, dominic.tildesley@epfl.ch ("the authors").
+  ! This software was written in 2016/17
+  ! by Michael P Allen, m.p.allen@warwick.ac.uk, m.p.allen@bristol.ac.uk
+  ! and Dominic J Tildesley, dominic.tildesley@epfl.ch ("the authors"),
+  ! to accompany the book "Computer Simulation of Liquids", second edition, 2017 ("the text"),
+  ! published by Oxford University Press ("the publishers").
   !
   ! LICENCE
   ! Creative Commons CC0 Public Domain Dedication
@@ -10,7 +13,8 @@
   ! If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
   !
   ! DISCLAIMER
-  ! The authors make no warranties about the software, and disclaim liability for all uses of the software,
-  ! to the fullest extent permitted by applicable law.
-  ! The authors do not recommend use of this software for any purpose.
-  ! When using or citing the software, you should not imply endorsement by the authors.
+  ! The authors and publishers make no warranties about the software, and disclaim liability
+  ! for all uses of the software, to the fullest extent permitted by applicable law.
+  ! The authors and publishers do not recommend use of this software for any purpose.
+  ! It is made freely available, solely to clarify points made in the text.
+  ! When using or citing the software, you should not imply endorsement by the authors or publishers.
