4ab4ebe6532c27bc651310390660d191f59976d1
Mike <m.p.allen@warwick.ac.uk>
2017-03-31 22:38:36 +0200

    Added torques to t_tensor.f90 example

--- t_tensor.f90
+++ t_tensor.f90
@@ -33,11 +33,11 @@ PROGRAM t_tensor
 
   USE, INTRINSIC :: iso_fortran_env, ONLY : input_unit, output_unit, error_unit, iostat_end, iostat_eor
 
-  USE maths_module, ONLY : init_random_seed, random_vector, outer_product
+  USE maths_module, ONLY : init_random_seed, random_vector, outer_product, cross_product
 
   IMPLICIT NONE
 
-  REAL, DIMENSION(3)         :: r12, r12_hat, e1, e2, mu1, mu2, f12t, f12e, vec
+  REAL, DIMENSION(3)         :: r12, r12_hat, e1, e2, mu1, mu2, f12t, f12e, t1t, t2t, t1e, t2e, vec
   REAL, DIMENSION(3,3)       :: tt2, quad1, quad2, mat
   REAL, DIMENSION(3,3,3)     :: tt3
   REAL, DIMENSION(3,3,3,3)   :: tt4, e4
@@ -68,11 +68,11 @@ PROGRAM t_tensor
   ! Comment out, or replace, this section if you don't like namelists
   READ ( unit=input_unit, nml=nml, iostat=ioerr )
   IF ( ioerr /= 0 ) THEN
-    WRITE ( unit=error_unit, fmt='(a,i15)') 'Error reading namelist nml from standard input', ioerr
-    IF ( ioerr == iostat_eor ) WRITE ( unit=error_unit, fmt='(a)') 'End of record'
-    IF ( ioerr == iostat_end ) WRITE ( unit=error_unit, fmt='(a)') 'End of file'
+     WRITE ( unit=error_unit, fmt='(a,i15)') 'Error reading namelist nml from standard input', ioerr
+     IF ( ioerr == iostat_eor ) WRITE ( unit=error_unit, fmt='(a)') 'End of record'
+     IF ( ioerr == iostat_end ) WRITE ( unit=error_unit, fmt='(a)') 'End of file'
      STOP 'Error in t_tensor'
- END IF
+  END IF
 
   ! Write out parameters
   WRITE ( unit=output_unit, fmt='(a,t40,f15.6)'  ) 'Min separation d_min',            d_min
@@ -112,69 +112,123 @@ PROGRAM t_tensor
   FORALL (i=1:3) quad2(i,i) = quad2(i,i) - 0.5
   quad2 = quad2_mag * quad2
 
+  ! The T tensors
   tt2 = t2_tensor ( r12_hat, r12_mag**3 ) ! Calculate the second rank tensor, T2
   tt3 = t3_tensor ( r12_hat, r12_mag**4 ) ! Calculate the third  rank tensor, T3
   tt4 = t4_tensor ( r12_hat, r12_mag**5 ) ! Calculate the fourth rank tensor, T4
   tt5 = t5_tensor ( r12_hat, r12_mag**6 ) ! Calculate the fifth  rank tensor, T5
 
   ! Headings
-  WRITE ( unit=output_unit, fmt='(/,t30,a36,t70,a36)' ) '.....Result from T tensor', '.....Result from Euler angles' 
+  WRITE ( unit=output_unit, fmt='(/,t30,a36,t70,a36)' ) '.....Result from T tensor', '.....Result from Euler angles'
+
+  WRITE ( unit=output_unit, fmt='(/,a)') 'Dipole-dipole'
 
   ! Calculate the dipole-dipole energy
   vec = MATMUL ( tt2, mu2 ) ! Contract T2 with dipole for molecule 2
   v12t = -DOT_PRODUCT ( mu1, vec ) ! and contract result with dipole for molecule 1
   v12e = mu1_mag * mu2_mag * ( c12 - 3.0 * c1 * c2 ) / r12_mag**3
-  WRITE ( unit=output_unit, fmt='(a,t30,f12.6,t70,f12.6)' ) 'Dipole-dipole energy =', v12t, v12e
+  WRITE ( unit=output_unit, fmt='(a,t30,f12.6,t70,f12.6)' ) 'Energy =', v12t, v12e
 
   ! Calculate the dipole-dipole force
   mat = outer_product ( mu1, mu2 ) ! Dyadic of the dipole vectors
-  vec = contract_32_1 ( tt3, mat )  ! Contract with T3
-  f12t = - vec
+  f12t = - contract_32_1 ( tt3, mat )  ! Contract with T3
   f12e = (3.0 * mu1_mag * mu2_mag / r12_mag**4 ) * ((c12 - 5.0 *c1 * c2) * r12_hat + c2 * e1 + c1 * e2 )
-  WRITE ( unit=output_unit, fmt='(a,t30,3f12.6,t70,3f12.6)' ) 'Dipole-dipole force  =', f12t, f12e
+  WRITE ( unit=output_unit, fmt='(a,t30,3f12.6,t70,3f12.6)' ) 'Force  =', f12t, f12e
+
+  ! Calculate the dipole-dipole torques
+  vec = -MATMUL ( tt2, mu2 ) ! Contract T2 with dipole for molecule 2
+  t1t = -cross_product ( mu1, vec ) ! Cross-product with dipole for molecule 1
+  t1e = -(mu1_mag * mu2_mag/ r12_mag**3)*cross_product(e1,( e2 - 3.0*c2*r12_hat ) )
+  WRITE ( unit=output_unit, fmt='(a,t30,3f12.6,t70,3f12.6)' ) 'Torque on 1  =', t1t, t1e
+  vec = -MATMUL ( tt2, mu1 ) ! Contract T2 with dipole for molecule 1
+  t2t = -cross_product ( mu2, vec ) ! Cross-product with dipole for molecule 1
+  t2e = -(mu1_mag * mu2_mag/ r12_mag**3)*cross_product(e2,( e1 - 3.0*c1*r12_hat ) )
+  WRITE ( unit=output_unit, fmt='(a,t30,3f12.6,t70,3f12.6)' ) 'Torque on 2  =', t2t, t2e
+
+  WRITE ( unit=output_unit, fmt='(/,a)') 'Dipole-quadrupole'
 
   ! Calculate the dipole-quadrupole energy
   vec = contract_32_1 ( tt3, quad2 ) ! Contract T3 with the quadrupole tensor for molecule 2
   v12t = -(1.0/3.0) * DOT_PRODUCT ( mu1, vec ) ! and contract with dipole for molecule 1
   v12e = 1.5 * mu1_mag * quad2_mag * ( c1 * (1.0 - 5.0 * c2 * c2) + 2.0 * c2 * c12 ) / r12_mag**4
-  WRITE ( unit=output_unit, fmt='(a,t30,f12.6,t70,f12.6)' ) 'Dipole-quadrupole energy =', v12t, v12e
-  
+  WRITE ( unit=output_unit, fmt='(a,t30,f12.6,t70,f12.6)' ) 'Energy =', v12t, v12e
+
   ! Calculate the dipole-quadrupole force
   mat  = contract_42_2 ( tt4, quad2 ) ! Contract T4 with the quadrupole tensor for molecule 2
   f12t = - (1.0/3.0) * MATMUL ( mu1, mat ) ! and contract result with dipole for molecule 1
   f12e = - (1.5 * mu1_mag * quad2_mag / r12_mag**5 ) * (( 35.0 * c1 * c2 * c2 - 10.0 * c2 * c12   &
-  - 5.0 * c1 ) * r12_hat + (1.0 - 5.0 * c2 * c2 ) * e1 + (2.0 * c12 - 10.0 * c1 * c2) * e2 )
-  WRITE ( unit=output_unit, fmt='(a,t30,3f12.6,t70,3f12.6)' ) 'Dipole-quadrupole force  =', f12t, f12e
+       - 5.0 * c1 ) * r12_hat + (1.0 - 5.0 * c2 * c2 ) * e1 + (2.0 * c12 - 10.0 * c1 * c2) * e2 )
+  WRITE ( unit=output_unit, fmt='(a,t30,3f12.6,t70,3f12.6)' ) 'Force  =', f12t, f12e
+
+  ! Calculate the dipole-quadrupole torques
+  vec = -(1.0/3.0)*contract_32_1 ( tt3, quad2 ) ! Contract T3 with the quadrupole tensor for molecule 2
+  t1t = -cross_product ( mu1, vec ) ! Cross-product with dipole for molecule 1
+  t1e = -(1.5*mu1_mag*quad2_mag/r12_mag**4)*cross_product ( e1, (1-5.0*c2**2)*r12_hat + 2.0 * c2*e2 )
+  WRITE ( unit=output_unit, fmt='(a,t30,3f12.6,t70,3f12.6)' ) 'Torque on 1  =', t1t, t1e
+  mat = -(1.0/3.0)*contract_31_2 ( tt3, mu1 ) ! Contract T3 with dipole tensor for molecule 1
+  mat = MATMUL ( quad2, mat ) ! Matrix multiply with quadrupole for molecule 2
+  t2t = -2.0*skew ( mat ) ! Get vector dual
+  t2e = -(3.0*mu1_mag*quad2_mag/r12_mag**4)*cross_product ( e2, (c12-5.0*c1*c2)*r12_hat + c2*e1 )
+  WRITE ( unit=output_unit, fmt='(a,t30,3f12.6,t70,3f12.6)' ) 'Torque on 2  =', t2t, t2e
+
+  WRITE ( unit=output_unit, fmt='(/,a)') 'Quadrupole-dipole'
 
   ! Calculate the quadrupole-dipole energy
   vec = contract_32_1 ( tt3, quad1 )  ! Contract T3 with the quadrupole tensor for molecule 1
   v12t  =  (1.0/3.0) * DOT_PRODUCT ( vec, mu2 ) ! and contract with dipole for molecule 2
   v12e  = - 1.5 * quad1_mag * mu2_mag * ( c2 * (1.0 - 5.0 * c1 * c1 ) + 2.0 * c1 * c12 ) / r12_mag**4
-  WRITE ( unit=output_unit, fmt='(a,t30,f12.6,t70,f12.6)' ) 'Quadrupole-dipole energy =', v12t, v12e
-  
+  WRITE ( unit=output_unit, fmt='(a,t30,f12.6,t70,f12.6)' ) 'Energy =', v12t, v12e
+
   ! Calculate the quadrupole-dipole force
   mat  = contract_42_2 ( tt4, quad1 ) ! Contract T4 with the quadrupole tensor for molecule 1 
   f12t = (1.0/3.0) * MATMUL ( mat, mu2 ) ! and contract result with dipole for molecule 2
   f12e = + (1.5 * mu2_mag * quad1_mag / r12_mag**5) * (( 35.0 * c1 * c1 * c2 - 10.0 * c1 * c12   &
-  - 5.0 * c2 ) * r12_hat + (1.0 - 5.0 * c1 * c1 ) * e2 + (2.0 * c12 - 10.0 * c1 * c2) * e1 )
-  WRITE ( unit=output_unit, fmt='(a,t30,3f12.6,t70,3f12.6)' ) 'Quadrupole-dipole force  =', f12t, f12e
- 
+       - 5.0 * c2 ) * r12_hat + (1.0 - 5.0 * c1 * c1 ) * e2 + (2.0 * c12 - 10.0 * c1 * c2) * e1 )
+  WRITE ( unit=output_unit, fmt='(a,t30,3f12.6,t70,3f12.6)' ) 'Force  =', f12t, f12e
+
+  ! Calculate the quadrupole-dipole torques
+  mat = (1.0/3.0)*contract_31_2 ( tt3, mu2 ) ! Contract T3 with dipole for molecule 2
+  mat = MATMUL ( quad1, mat ) ! Matrix multiply with quadrupole for molecule 1
+  t1t = -2.0*skew ( mat ) ! Get vector dual
+  t1e = (3.0*quad1_mag*mu2_mag/r12_mag**4)*cross_product(e1,(c12-5.0*c1*c2)*r12_hat + c1*e2)
+  WRITE ( unit=output_unit, fmt='(a,t30,3f12.6,t70,3f12.6)' ) 'Torque on 1  =', t1t, t1e
+  vec = (1.0/3.0)*contract_32_1 ( tt3, quad1 ) ! Contract T3 with quadrupole for molecule 1
+  t2t = -cross_product ( mu2, vec ) ! Cross product with dipole for molecule 2
+  t2e = (1.5*quad1_mag*mu2_mag/r12_mag**4)*cross_product(e2,(1-5.0*c1**2)*r12_hat + 2 * c1*e1)
+  WRITE ( unit=output_unit, fmt='(a,t30,3f12.6,t70,3f12.6)' ) 'Torque on 2  =', t2t, t2e
+
+  WRITE ( unit=output_unit, fmt='(/,a)') 'Quadrupole-quadrupole'
+
   ! Calculate the quadrupole-quadrupole energy
   mat  = contract_42_2 ( tt4, quad2 ) ! Contract T4 with the quadrupole tensor for molecule 2
   v12t = (1.0/9.0) * contract_22(quad1, mat ) ! and double dot with the quadrupole tensor for molecule 1
   v12e = 0.75 * quad1_mag * quad2_mag * ( 1.0 - 5.0 * c1 * c1 - 5.0 * c2 * c2 + 2.0 * c12 * c12 &
-         + 35.0 * c1 * c1 * c2 * c2 - 20.0 * c1 * c2 * c12 ) / r12_mag**5
-  WRITE ( unit=output_unit, fmt='(a,t30,f12.6,t70,f12.6)' ) 'Quadrupole-quadrupole energy =', v12t, v12e
-  
+       + 35.0 * c1 * c1 * c2 * c2 - 20.0 * c1 * c2 * c12 ) / r12_mag**5
+  WRITE ( unit=output_unit, fmt='(a,t30,f12.6,t70,f12.6)' ) 'Energy =', v12t, v12e
+
   ! Calculate the quadrupole-quadrupole force
   e4   = outer_22_4 ( quad1, quad2 ) ! Outer product of the quadrupole tensors
   f12t = (1.0/9.0) * contract_54_1 ( tt5, e4 ) ! Contract with T5
   f12e = (0.75 * quad1_mag * quad2_mag / r12_mag**6) * (( 5.0 - 35.0 * c1 * c1  - 35.0 * c2 * c2  &
-          + 10.0 * c12 * c12 + 315.0 * c1 * c1 * c2 * c2 - 140.0 * c1 * c2 * c12 ) * r12_hat  &
-          + ( 10.0 * c1 - 70.0 * c1 * c2 * c2 + 20.0 * c2 * c12 ) * e1                        &
-          + ( 10.0 * c2 - 70.0 * c2 * c1 * c1 + 20.0 * c1 * c12 ) * e2   )          
-  WRITE ( unit=output_unit, fmt='(a,t30,3f12.6,t70,3f12.6)' ) 'Quadrupole-quadrupole force  =', f12t, f12e
-  
+       + 10.0 * c12 * c12 + 315.0 * c1 * c1 * c2 * c2 - 140.0 * c1 * c2 * c12 ) * r12_hat  &
+       + ( 10.0 * c1 - 70.0 * c1 * c2 * c2 + 20.0 * c2 * c12 ) * e1                        &
+       + ( 10.0 * c2 - 70.0 * c2 * c1 * c1 + 20.0 * c1 * c12 ) * e2   )          
+  WRITE ( unit=output_unit, fmt='(a,t30,3f12.6,t70,3f12.6)' ) 'Force  =', f12t, f12e
+
+  ! Calculate the quadrupole-quadrupole torques
+  mat = (1.0/9.0)*contract_42_2 ( tt4, quad2 ) ! Contract T4 with the quadrupole tensor for molecule 2
+  mat = MATMUL ( quad1, mat ) ! Matrix multiply with quadrupole for molecule 1
+  t1t = -2.0*skew(mat) ! Get vector dual
+  t1e = -(3.0*quad1_mag*quad2_mag/r12_mag**5)* &
+       &   cross_product(e1,2.5*(c1*(7.0*c2**2-1.0)-2.0*c2*c12)*r12_hat - (5.0*c1*c2-c12)*e2)
+  WRITE ( unit=output_unit, fmt='(a,t30,3f12.6,t70,3f12.6)' ) 'Torque on 1  =', t1t, t1e
+  mat = (1.0/9.0)*contract_42_2 ( tt4, quad1 ) ! Contract T4 with the quadrupole tensor for molecule 1
+  mat = MATMUL ( quad2, mat ) ! Matrix multiply with quadrupole for molecule 2
+  t2t = -2.0*skew(mat) ! Get vector dual
+  t2e = -(3.0*quad1_mag*quad2_mag/r12_mag**5)* &
+       & cross_product(e2,2.5*(c2*(7.0*c1**2-1.0)-2.0*c1*c12)*r12_hat -(5.0*c1*c2-c12)*e1)
+  WRITE ( unit=output_unit, fmt='(a,t30,3f12.6,t70,3f12.6)' ) 'Torque on 2  =', t2t, t2e
+
 CONTAINS
 
   FUNCTION t2_tensor ( r, r3 ) RESULT ( t2 )
@@ -219,7 +273,7 @@ CONTAINS
     t3 = t3 / r4 ! Scale by fourth power of distance
 
   END FUNCTION t3_tensor
-  
+
   FUNCTION t4_tensor ( r, r5 ) RESULT ( t4 )
     IMPLICIT NONE
     REAL, DIMENSION(3,3,3,3)            :: t4 ! Returns fourth-rank 3x3x3x3 interaction tensor
@@ -250,7 +304,7 @@ CONTAINS
     t4 = t4 / r5 ! Scale by fifth power of distance
 
   END FUNCTION t4_tensor
-  
+
   FUNCTION t5_tensor ( r, r6 ) RESULT ( t5 )
     IMPLICIT NONE
     REAL, DIMENSION(3,3,3,3,3)          :: t5 ! Returns fifth-rank 3x3x3x3X3 interaction tensor
@@ -290,16 +344,32 @@ CONTAINS
     t5 = t5 / r6 ! Scale by sixth power of distance
 
   END FUNCTION t5_tensor
-  
+
   FUNCTION contract_22 ( a, b ) RESULT ( c )
     IMPLICIT NONE
     REAL                              :: c ! Returns a zero-rank contraction
     REAL, DIMENSION(3,3), INTENT(in)  :: a ! of a second-rank tensor
     REAL, DIMENSION(3,3), INTENT(in)  :: b ! with another second-rank tensor
-    
+
     c = SUM ( a * b )
-    
+
   END FUNCTION contract_22
+
+  FUNCTION contract_31_2 ( a, b ) RESULT ( c )
+    IMPLICIT NONE
+    REAL, DIMENSION(3,3)                :: c ! Returns a second-rank contraction of 
+    REAL, DIMENSION(3,3,3), INTENT(in)  :: a ! a third-rank tensor
+    REAL, DIMENSION(3),     INTENT(in)  :: b ! and a first-rank tensor
+
+    INTEGER :: i, j
+
+    DO i = 1, 3
+       DO j = 1, 3
+          c(i,j) = SUM ( a(i,j,:) * b(:) )
+       END DO
+    END DO
+
+  END FUNCTION contract_31_2
 
   FUNCTION contract_32_1 ( a, b ) RESULT ( c )
     IMPLICIT NONE
@@ -314,7 +384,7 @@ CONTAINS
     END DO
 
   END FUNCTION contract_32_1
-  
+
   FUNCTION contract_42_2 ( a, b ) RESULT ( c )
     IMPLICIT NONE
     REAL, DIMENSION(3,3)                 :: c ! Returns a second-rank contraction of 
@@ -330,7 +400,7 @@ CONTAINS
     END DO
 
   END FUNCTION contract_42_2
-  
+
   FUNCTION contract_54_1 ( a, b ) RESULT ( c )
     IMPLICIT NONE
     REAL, DIMENSION(3)                     :: c ! Returns a first-rank contraction of 
@@ -342,9 +412,9 @@ CONTAINS
     DO i = 1, 3    
        c(i) = SUM ( a(i,:,:,:,:) * b(:,:,:,:) ) 
     END DO
-    
-  END FUNCTION contract_54_1 
-   
+
+  END FUNCTION contract_54_1
+
   FUNCTION outer_22_4 ( a, b ) RESULT( c )
 
     IMPLICIT NONE
@@ -365,5 +435,17 @@ CONTAINS
     END DO
 
   END FUNCTION outer_22_4
+
+  FUNCTION skew ( a ) RESULT ( b )
+
+    IMPLICIT NONE
+    REAL, DIMENSION(3)   :: b ! Returns a vector by doubly contracting Levi-Civita symbol with
+    REAL, DIMENSION(3,3) :: a ! a second-rank tensor
+
+    b(1) = a(2,3) - a(3,2)
+    b(2) = a(3,1) - a(1,3)
+    b(3) = a(1,2) - a(2,1)
+
+  END FUNCTION skew
 
 END PROGRAM t_tensor
